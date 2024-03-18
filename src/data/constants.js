46588dc5f39e2af7d2ae0ac0cf344a665d842317
sayaliburtepersonal <sayaliburte8march@gmail.com>
2024-03-18 14:16:35 +0530

    1)Created project and added basic folder structure. 2)Created different components like skills,eduction,etc.

---
+++ src/data/constants.js
@@ -0,0 +1,10 @@
+export const Bio = {
+  name: "Sayali Burte",
+  roles: ["Frontend Developer", "Angular Developer", "React Developer"],
+  description:
+    "Highly motivated Frontend Developer with 8 months of professional experience in creating engaging and interactive web applications using Angular. Additionally, completed 6 months of internship experience in React development. Experience in Angular, TypeScript, HTML, CSS, SCSS. My ultimate goal is to continuously evolve and excel as a Full Stack Developer.",
+  github: "https://github.com/sayaliburte",
+  resume:
+    "https://drive.google.com/file/d/1cqxpe9MslghjZQ_es5-YYB25zwzN6PMK/view",
+  linkedin: "https://www.linkedin.com/in/sayali-burte/",
+};
