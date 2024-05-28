fdc4b15874d2c8bbd7a167e3c7252264e3a84c3e
sayaliburtepersonal <sayaliburte8march@gmail.com>
2024-05-28 19:01:45 +0530

    1)Added Skills array to display skills data.2)Added skills section to display skills and added css to it.

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,9 +1,142 @@
 import React from "react";
+import { skills } from "../../data/constants";
+import styled from "styled-components";
+const Skills = () => {
+  const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    z-index: 1;
+  `;
+  const Wrapper = styled.div`
+    max-width: 1100px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    gap: 12px;
+  `;
 
-const Skills=()=>{
-    return(<div>
-        Skills
-    </div>)
-}
+  const Title = styled.h1`
+    font-size: 42px;
+    font-weight: 600;
+    text-align: center;
+    margin-top: 20px;
+    color: ${({ theme }) => theme.text_primary};
+
+    @media (max-width: 768px) {
+      margin-top: 12px;
+      font-size3: 32px;
+    }
+  `;
+
+  const Desc = styled.div`
+    font-size: 18px;
+    max-width: 600px;
+    text-align: center;
+    color: ${({ theme }) => theme.text_secondary};
+    @media (max-width: 768px) {
+      font-size: 16px;
+    }
+  `;
+
+  const SkillsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 30px;
+  gap: 30px;
+  justify-content: center;
+`
+
+const Skill = styled.div`
+  width: 100%;
+  max-width: 400px;
+  background: ${({ theme }) => theme.card};
+  border: 0.1px solid #854CE6;
+  box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
+  border-radius: 16px;
+  padding: 18px 36px;
+  @media (max-width: 768px) {
+    max-width: 400px;
+    padding: 10px 36px;
+  }
+  @media (max-width: 500px) {
+    max-width: 330px;
+    padding: 10px 36px;
+  }
+
+
+`
+
+const SkillTitle = styled.h2`
+  font-size: 28px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_secondary};
+  margin-bottom: 20px;
+  text-align: center;
+`
+
+const SkillList = styled.div`
+  display: flex;
+  justify-content: center; 
+  flex-wrap: wrap;
+  gap: 12px;
+  margin-bottom: 20px;
+`
+
+const SkillItem = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_primary + 80};
+  border: 1px solid ${({ theme }) => theme.text_primary + 80};
+  border-radius: 12px;
+  padding: 12px 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  @media (max-width: 768px) {
+    font-size: 14px;
+    padding: 8px 12px;
+  }
+  @media (max-width: 500px) {
+    font-size: 14px;
+    padding: 6px 12px;
+  }
+`
+
+const SkillImage = styled.img`
+  width: 24px;
+  height: 24px;
+`
+  return (
+    <Container id="skills ">
+      <Wrapper>
+        <Title>Skills</Title>
+        <Desc>Mentioned below are some of my skills</Desc>
+        <SkillsContainer>
+          {skills.map((item) => (
+            <Skill>
+              <SkillTitle>{item.title}</SkillTitle>
+              <SkillList>
+                {item.skills.map((skill) => (
+                  <SkillItem>
+                    <SkillImage src={skill.image}></SkillImage>
+                    {skill.name}
+                  </SkillItem>
+                ))}
+              </SkillList>
+            </Skill>
+          ))}
+        </SkillsContainer>
+      </Wrapper>
+    </Container>
+  );
+};
 
 export default Skills;
--- src/data/constants.js
+++ src/data/constants.js
@@ -5,6 +5,106 @@ export const Bio = {
     "Highly motivated Frontend Developer with 8 months of professional experience in creating engaging and interactive web applications using Angular. Additionally, completed 6 months of internship experience in React development. Experience in Angular, TypeScript, HTML, CSS, SCSS. My ultimate goal is to continuously evolve and excel as a Full Stack Developer.",
   github: "https://github.com/sayaliburte",
   resume:
-    "https://drive.google.com/file/d/1cqxpe9MslghjZQ_es5-YYB25zwzN6PMK/view",
+    "https://drive.google.com/file/d/1_9Wn1_EIePVhH9O8Y4wAnNWkNj11sY_U/view",
   linkedin: "https://www.linkedin.com/in/sayali-burte/",
 };
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Angular",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png",
+      },
+      {
+        name: "React Native",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6CL-wlw0CN2PuGYrYN1fa8_6FyXM1t5YI9D4Gba4sVg&s",
+      },
+    ],
+  },
+  {
+    title: "Frontend Designing",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "Bootstrap 5",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Programming Languages",
+    skills: [
+      {
+        name: "JavaScript (ES6)",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Typescript",
+        image:
+          "https://cdn.iconscout.com/icon/free/png-256/free-typescript-3521774-2945272.png",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+    ],
+  },
+  {
+    title: "Database",
+    skills: [
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Git",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp3dMZHVV51klEwe2rRRLScE0ggEnGwnYJAibEVnuyBUMV4LMt-QlCdGLmq799laaaQ1w&usqp=CAU",
+      },
+      {
+        name: "Webstrom",
+        image:
+          "https://w7.pngwing.com/pngs/495/445/png-transparent-webstorm-icon-hd-logo-thumbnail.png",
+      },
+      {
+        name: "Jira",
+        image:
+          "https://w7.pngwing.com/pngs/992/738/png-transparent-jira-hd-logo-thumbnail.png",
+      },
+    ],
+  },
+];
